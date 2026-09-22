954389ae61b30a2a3b2e7040a30bc4ce33f84b75
neckeel <paolla.neckel.silva@escola.pr.gov.br>
2026-09-22 11:45:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-constnumeroSenha = document.querySelector('.parametro-senha__textos');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas =  'abcdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789';
-const simbolos = '!@#*?*%';
-const botoes = document.querySelectorAll('parametro-senha_-botao');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
+const numeros = '0123456789'
+const simbolos = '!@%*?#'
+const botoes = document.querySelectorAll('parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 const forcaSenha = document.querySelector('forca');
 
-botoes[0].onclik = diminuiTamanho;
-botoes[1].onclik = aumentaTamanho;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if(tamanhoSenha> 1){
+function diminuiTamanho (){
+    if(tamanhoSenha > 1){
         //tamanhoSenha = tamanhoSenha-1;
-        tamanhosenha--;
+        tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
 function aumentaTamanho(){
-    if(tamanhoSenha< 20){
+    if(tamanhoSenha < 20){
         //tamanhoSenha = tamanhoSenha+1;
         tamanhoSenha++;
     }
@@ -31,33 +31,88 @@ function aumentaTamanho(){
     geraSenha();
 }
 
-for(i=0; i< checkbox.length;i++){
-    checkbox[i].onclik = geraSenha;
+for(i = 0; i < checkbox.length;i++);{
+    checkbox[i].onclick = geraSenha;
 }
 
-geraSenha();
-function geraSenha(){
+geraSenha();{
     let alfabeto = ";
     if(checkbox[0].cheked){
-    alfabeto = alfabeto + letrasMaiusculas;
+        alfabeto = alfabeto + letrasMaiusculas;
 }
-if(checkbox[1].cheked){
+
+if (checkbox[1].cheked){
     alfabeto = alfabeto + letrasMinusculas;
 }
-if(checkbox[2].cheked){
+if (checkbox[2].cheked){
     alfabeto = alfabeto + numeros;
 }
-if(checkbox[3].cheked){
+ if (checkbox[3].cheked){
     alfabeto = alfabeto + simbolos;
+ }
+
+ let senha = '';
+ for(let i=0;i<tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
+ }
+
+ campoSenha.value = senha;
+ classifiqueSenha(alfabeto.length);
+ 
 }
 
-let senha = ";
-for(let i=0;i<tamanhoSenha;i++){
-let numeroSenha = math.random()*alfabeto.length;
-numeroAleatorio = math.floor(numeroAleatorio);
-senha = senha + alfabeto[numeroAleatorio];
-}
+function classificaSenha(tamanhoAlfabeto) {
 
-campoSenha.value = senha;
-clasificasenha(alfabeto.length);
+
+    let entropia =
+tamanhoSenha * Math.log2(tamanhoAlfabeto);
+
+
+   
+console.log(entropia);
+
+
+   
+forcaSenha.classList.remove('fraca', 'media', 'forte');
+
+
+    if (entropia >
+57) {
+
+
+       
+forcaSenha.classList.add('forte');
+
+
+    } else if
+(entropia > 35 && entropia < 57) {
+
+
+       
+forcaSenha.classList.add('media');
+
+
+    } else if
+(entropia <= 35) {
+
+
+       
+forcaSenha.classList.add('fraca');
+
+
+    }
+
+
+    const
+valorEntropia = document.querySelector('.entropia');
+
+
+   
+valorEntropia.textContent = "Um computador pode levar até " +
+Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir
+essa senha.";
+
+
 }
